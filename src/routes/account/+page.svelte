5db0ab14c8986130812578c5fb1ea7d9a4617379
Enderboy2 <y01006452923@gmail.com>
2024-06-22 22:18:25 +0300

    .

--- src/routes/account/+page.svelte
+++ src/routes/account/+page.svelte
@@ -33,7 +33,7 @@
 {#if selected_syllabus === "NULL"}
 
 <div>
-	<h1>Hi {username}, Welcome to Whizz!</h1>
+	<h1 class="font-space">Hi {username}, Welcome to Whizz!</h1>
 	<h2>I see you hevent selected your subjects yet, care to do so?</h2>
 	
 </div>
